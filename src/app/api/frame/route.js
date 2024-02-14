eb4290236d5668faf5a2a5fd1882130e47b1da19
Sujin K <lksujins@gmail.com>
2024-02-14 17:45:23 +0530

    image id fix

--- src/app/api/frame/route.js
+++ src/app/api/frame/route.js
@@ -10,7 +10,7 @@ async function getResponse(req) {
     return new NextResponse(`<!DOCTYPE html><html><head>
   <title>This is frame 7</title>
   <meta property="fc:frame" content="vNext" />
-  <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/frame4.png" />
+  <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmNpypEw4JnKAZF6aeinPMpLWByrdzyUdRAn4iw2nZemQT/frame4.png" />
   <meta property="fc:frame:button:1" content="Cosmic Cowboys" />
   <meta property="fc:frame:button:1:action" content="post_redirect" />
   <meta property="fc:frame:button:2" content="Blog post Tutorial" />
